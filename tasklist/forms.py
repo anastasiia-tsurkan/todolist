613fa76e8ae66f69895b53be451446d51218285c
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 22:52:24 +0300

    Add Tag form

--- tasklist/forms.py
+++ tasklist/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Task
+from .models import Task, Tag
 
 
-class TaskForm(forms.Form):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
+        fields = "__all__"
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
         fields = "__all__"
