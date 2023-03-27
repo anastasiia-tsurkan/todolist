91a11e21a2df362fcf6b7c653d75b225a7d56945
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 22:53:44 +0300

    Add __str__ method to existing models

--- tasklist/models.py
+++ tasklist/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
@@ -11,3 +14,6 @@ class Task(models.Model):
     deadline = models.DateTimeField(blank=True)
     status = models.BooleanField()
     tags = models.ManyToManyField(Tag, related_name="tags")
+
+    def __str__(self):
+        return self.content
