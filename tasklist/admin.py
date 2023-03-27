52293e69e2c4615a82eedfa57af734c2e4d4b33c
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 17:58:04 +0300

    Add admin models

--- tasklist/admin.py
+++ tasklist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "datetime", "deadline", "done")
+
+
+admin.site.register(Tag)
