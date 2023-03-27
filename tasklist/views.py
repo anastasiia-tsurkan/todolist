c6a8a37769364fc3f8fdd0411366d790c59b7a86
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 20:11:38 +0300

    Create TaskForm form

--- tasklist/views.py
+++ tasklist/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Task
+from .forms import TaskForm
 
 
 # def index(request):
@@ -19,13 +20,13 @@ class TaskList(generic.ListView):
 
 class TaskCreate(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("tasklist:index")
 
 
 class TaskUpdate(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("tasklist:index")
 
 
