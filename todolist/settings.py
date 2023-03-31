458f1926ce485626adb784c7ce1dcb27acd1491c
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-31 14:20:45 +0300

    Change secret_key default

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRETE_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-!b=28p9(k8zgm$%i#qr5cd4924c^)+3q$rmrq$t6yuqc#l@@!p")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
